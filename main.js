ad8f19ba004314dcbe381b6c1deae88edd5a519f
otavionascimentosilva-sketch <otavio.nascimento.silva@escola.pr.gov.br>
2026-05-12 07:55:11 -0300

    .

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const botoes = document.querySelectorAll(".botao")
 console.log(botoes)
+const textos = document.querySelectorAll(".aba-conteudo")
+console.log(textos)
+  
+for(let i = 0; i<botoes.length;i++){
+    botoes[i].onclick = function () {
+        for(let j=0; j<botoes.length;j++){
+            botoes[i].classList.add("ativo")
+            textos[i].classList.add("ativo")
+        }
+    }
+}
